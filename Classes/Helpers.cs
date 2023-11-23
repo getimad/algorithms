8d597adf80f4ce7245ac0ef1c51ca593e217f794
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-11-23 13:22:01 +0100

    Add Classes\, Helpers.cs and Sort.cs
    - Add Swap method to Helpers.cs.
    - Add Bubble method to Sort.cs.

---
+++ Classes/Helpers.cs
@@ -0,0 +1,8 @@
+﻿namespace algorithms.Classes
+{
+    internal static class Helpers
+    {
+        public static void Swap(ref int num1, ref int num2)
+            => (num1, num2) = (num2, num1);
+    }
+}
---
+++ Classes/Sort.cs
@@ -0,0 +1,19 @@
+﻿namespace algorithms.Classes
+{
+    internal static class Sort
+    {
+        public static void Bubble(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                        Helpers.Swap(ref arr[i], ref arr[j]);
+                }
+            }
+        }
+
+
+    }
+}
