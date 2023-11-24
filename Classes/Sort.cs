27c70d38ac20630e947c1b4e34a07f6c72332901
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-11-24 22:54:28 +0100

    Add Insertion method to Sort.cs

--- Classes/Sort.cs
+++ Classes/Sort.cs
@@ -29,5 +29,22 @@
                 Helpers.Swap(ref arr[indexOfMin], ref arr[i]);
             }
         }
+
+        public static void Insertion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int current = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && current < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = current;
+            }
+        }
     }
 }
