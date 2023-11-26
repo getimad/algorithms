f434749c13250e54e58b23eec23dde96a8fdba62
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-11-26 22:23:43 +0100

    Add Search.cs
    - Add Linear method to Search.cs

---
+++ Classes/Search.cs
@@ -0,0 +1,16 @@
+﻿namespace algorithms.Classes
+{
+    internal static class Search
+    {
+        public static int Linear(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
