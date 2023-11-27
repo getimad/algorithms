e2d398c3cfbb0ceb80e735b6fb634ad4c80ab33c
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-11-27 15:17:55 +0100

    Add Binary method to Search.cs

--- Classes/Search.cs
+++ Classes/Search.cs
@@ -12,5 +12,29 @@
 
             return -1;
         }
+
+        public static int Binary(int[] arr, int target, bool isSorted = false)
+        {
+            if (!isSorted)
+                Array.Sort(arr);
+
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (arr[middle] == target)
+                    return middle;
+
+                if (target > arr[middle])
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+
+            return -1;
+        }
     }
 }
